da297fb4ba9137dbca6fe548b866a5236fec3782
qbao <dangquocbao9504@gmail.com>
2026-02-18 10:34:26 +0700

    add github discussions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,4 +24,5 @@ export default defineConfig({
       },
     },
   },
+  integrations: [react()],
 });
---
+++ src/components/Comment.tsx
@@ -0,0 +1,20 @@
+import Giscus from '@giscus/react'
+
+const Comments = () => {
+  return (
+    <Giscus
+      repo="livensmi1e/space"
+      repoId="R_kgDONjwlNQ"
+      categoryId="DIC_kwDONjwlNc4C2rAt"
+      mapping="pathname"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      lang="en"
+      loading="lazy"
+      strict="0"
+    />
+  )
+}
+
+export default Comments
